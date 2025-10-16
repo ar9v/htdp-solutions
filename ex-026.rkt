5db85d90270a23c0b1fc0474b62e20414a6ba454
Ricardo Vela <aricav96@gmail.com>
2025-10-15 22:13:34 -0600

    ex-026.rkt

---
+++ ex-026.rkt
@@ -0,0 +1,21 @@
+#lang htdp/bsl
+
+;; What do you expect as the valu of this program?
+(define (string-insert s i)
+  (string-append (substring s 0 i)
+                 "_"
+                 (substring s i)))
+
+(string-insert "helloworld" 6)
+
+;; => "hellow_orld"
+;;
+;; (string-insert "helloworld" 6)
+;; ===
+;; (string-append (substring "hellloworld" 0 6) "_" (substring "helloworld" 6))
+;; ===
+;; (string-append "hellow" "_" (substring "helloworld" 6))
+;; ===
+;; (string-append "hellow" "_" "orld")
+;; ===
+;; "hellow_orld"
