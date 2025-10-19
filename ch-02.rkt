01a72b3fe5b03ffb52873774cf1c2f9a93c5e59f
Ricardo Vela <aricav96@gmail.com>
2025-10-18 22:09:04 -0600

    ch-02.rkt: Add 2.3 relevant code

--- ch-02.rkt
+++ ch-02.rkt
@@ -1,6 +1,31 @@
 #lang htdp/bsl
 
-;; 2.3 Sample Problem
+;; 2.3
+(define (letter fst lst signature-name)
+  (string-append
+   (opening fst)
+   "\n\n"
+   (body fst lst)
+   "\n\n"
+   (closing signature-name)))
+
+(define (opening fst)
+  (string-append "Dear " fst ","))
+
+(define (body fst lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " lst " have won our lottery. So, " "\n"
+   fst ", " "hurry and pick up your prize."))
+
+(define (closing signature-name)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+   signature-name
+   "\n"))
+
+;;; Sample Problem
 ;;; The owner of a monopolistic movie theater in a small town has complete freedom in
 ;;; setting ticket prices. Teh more he charges, the fewer people can afford tickets.
 ;;; The less he charges, the more it costs to run a show because attendance goes up.
