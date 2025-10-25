02ef807dee3843b9a64774a3f64b413a411240e4
Ricardo Vela <aricav96@gmail.com>
2025-10-24 22:22:40 -0600

    helpers.el: rework find-exercise commands, add support for org
    Reworked this so `find-exercise` could account for org files, but also
    added `find-next-exercise`, which will open a file for the exercise
    that comes after the latest one.

--- helpers.el
+++ helpers.el
@@ -1,6 +1,55 @@
 ;; These can be loaded with `M-x load-file` as needed
 
-(defun htdp/find-exercise (n)
+;;; Exercise: (Number . Filename)
+(defun htdp/exercise (n-or-ex-filename &optional org)
+  (cond ((and (stringp n-or-ex-filename)
+              (htdp/exercise-filename? n-or-ex-filename))
+         (cons (htdp/exercise-filename->number n-or-ex-filename)
+               n-or-ex-filename))
+        ((numberp n-or-ex-filename)
+         (cons n-or-ex-filename
+               (htdp/number->exercise-filename n-or-ex-filename org)))))
+
+(defun htdp/exercise-number (exercise) (car exercise))
+(defun htdp/exercise-filename (exercise) (cdr exercise))
+
+(defun htdp/exercise-filename? (string)
+  (string-match "ex-[[:digit:]]\\{3\\}.[rkt\\|org]" string))
+
+(defun htdp/exercise-filename->number (exercise-filename)
+  (string-to-number (elt (string-split exercise-filename "[-,.]")
+                         1)))
+
+(defun htdp/number->exercise-filename (n &optional org)
+  (format "ex-%s.%s"
+          (string-pad (number-to-string n) 3 ?0 t)
+          (if org "org" "rkt")))
+
+(defun htdp/exercise-filename-for (n) (alist-get n (htdp/all-exercises)))
+(defun htdp/latest-exercise () (car (last (htdp/all-exercises))))
+(defun htdp/all-exercises ()
+  (let* ((all-files (sort (project-files (project-current))))
+         (ex-filenames (seq-filter #'htdp/exercise-filename? all-files)))
+    (seq-map #'htdp/exercise ex-filenames)))
+
+(defun htdp/in-htdp-dir? ()
+  "True if the `project-current' is a path with 'htdp' in it."
+
+  (let* ((project (or (project-current) (error "Not in a project")))
+         (directory (directory-file-name (project-root project))))
+
+    (string= (car (last (file-name-split directory))) "htdp")))
+
+(defun htdp/find-next-exercise (&optional org)
+  (interactive "P")
+
+  (unless (htdp/in-htdp-dir?) (error "Not in HtDP project!"))
+
+  (find-file (htdp/exercise-filename
+              (htdp/exercise (1+ (htdp/exercise-number (htdp/latest-exercise)))
+                             org))))
+
+(defun htdp/find-exercise (n org)
   "HtDP-specific `find-file', which builds the filename. Given `N', it'll build a
 filename of the form `ex-%s.rkt', where `%s' will be padded with zeroes accordingly. This
 command will fail if run from a directory which is not named `htdp'.
@@ -15,18 +64,17 @@ E.g.:
 There are 528 exercises in the book, so this assumes we'll pad up to 2 spaces.
 It doesn't validate that n <= 528, however."
 
-  (interactive "nExercise number: " racket-mode)
-
+  (interactive "nExercise number: \nP")
   (require 'f)
 
-  (if (string= (f-base default-directory) "htdp")
-      (let* ((max-digits 3)
-             (number-string (number-to-string n))
-             (filename (format "ex-%s.rkt" (string-pad number-string max-digits ?0 t))))
-          (find-file filename))
-    (error "Not in the htdp/ directory!")))
+  (unless (htdp/in-htdp-dir?) (error "Not in HtDP project!"))
 
-(keymap-set racket-xp-mode-map "C-c C-h" 'htdp/find-exercise)
+  (let ((maybe-exercise-filename (htdp/exercise-filename-for n)))
+    (find-file (or maybe-exercise-filename
+                   (htdp/exercise-filename (htdp/exercise n org))))))
+
+(keymap-set project-prefix-map "h" 'htdp/find-exercise)
+(keymap-set project-prefix-map "n" 'htdp/find-next-exercise)
 
 (defun htdp/insert-evaluation-steps (form)
   "Insert `FORM' into the buffer as a comment, then a newline, and then a separator
