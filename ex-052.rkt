6420dfd5629ce6aad153b6017b6da57c3ee9727e
Ricardo Vela <aricav96@gmail.com>
2025-11-07 21:44:16 -0600

    ex-052.rkt

---
+++ ex-052.rkt
@@ -0,0 +1,15 @@
+#lang htdp/bsl
+
+;;; [3, 5]
+;;; (3, 5]
+;;; [3, 5)
+;;; (3, 5)
+;;;
+;;; Which integers are contained in the four intervals above?
+
+;;; A:
+;;;
+;;; [3, 5] == 3, 4, 5
+;;; (3, 5] == 4, 5
+;;; [3, 5) == 3, 5
+;;; (3, 5) == 4
