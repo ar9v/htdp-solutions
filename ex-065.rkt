3e9c8efe5a434a2499a32a3779612626cb0d9b4e
Ricardo Vela <aricav96@gmail.com>
2025-11-12 21:58:02 -0600

    ex-065.rkt

---
+++ ex-065.rkt
@@ -0,0 +1,30 @@
+#lang htdp/bsl
+
+;; Take a look at the following structure type definitions. Write down
+;; the names of the functions (constructors, selectors, and predicates)
+;; that each introduces
+
+;; (define-struct movie [title producer year])
+;;; make-movie
+;;; movie-title, movie-producer, movie-year
+;;; movie?
+
+;; (define-struct person [name hair eyes phone])
+;;; make-person
+;;; person-name, person-hair, person-eyes, person-phone
+;;; person?
+
+;; (define-struct pet [name number])
+;;; make-pet
+;;; pet-name, pet-number
+;;; pet?
+
+;; (define-struct CD [artist title price])
+;;; make-CD
+;;; CD-artist, CD-title, CD-price
+;;; CD?
+
+;; (define-struct sweater [material size producer])
+;;; make-sweater
+;;; sweater-material, sweater-size, sweater-producer
+;;; sweater?
