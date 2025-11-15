9e91da09898cbc9607a1ba1957d2565e095406d5
Ricardo Vela <aricav96@gmail.com>
2025-11-14 18:24:02 -0600

    ex-071.rkt

---
+++ ex-071.rkt
@@ -0,0 +1,35 @@
+#lang htdp/bsl
+
+;;; Place the following definitions into DrRacket's definitions area:
+
+; distances in terms of pixels:
+(define HEIGHT 200)
+(define MIDDLE (quotient HEIGHT 2))
+(define WIDTH 400)
+(define CENTER (quotient WIDTH 2))
+
+(define-struct game [left-player right-player ball])
+
+(define game0
+  (make-game MIDDLE MIDDLE (make-posn CENTER CENTER)))
+
+;;; Click RUN and evaluate the following expressions
+(game-ball game0)
+;; (game-ball game0)
+;; ==
+;; (game-ball (make-game 100 100 (make-posn 200 200)))
+;; ==
+;; (make-posn 200 200)
+
+(posn? (game-ball game0))
+;; (posn? (game-ball game0))
+;; ==
+;; (posn? (game-ball (make-game 100 100 (make-posn 200 200))))
+;; ==
+;; (posn? (make-posn 200 200))
+;; #true
+
+(game-left-player game0)
+;; (game-left-player game0)
+;; ==
+;; 100
