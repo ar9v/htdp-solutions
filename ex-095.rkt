b2e5ba6995519351dde0beb5ebe6f289dc041f20
Ricardo Vela <aricav96@gmail.com>
2025-11-22 21:13:11 -0600

    ex-095.rkt

---
+++ ex-095.rkt
@@ -0,0 +1,7 @@
+#lang htdp/bsl
+
+;;; Explain why the three instances are generated according to the first or second
+;;; clause of the data definition.
+
+;;; A: Because SIGS is an enumeration! Both of them represent a way of creating a valid
+;;; SIGS instance.
