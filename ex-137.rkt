4f896d1e985480e4133b6777a719aaf3365e14d6
Ricardo Vela <aricav96@gmail.com>
2025-12-02 20:36:04 -0600

    ex-137.rkt

---
+++ ex-137.rkt
@@ -0,0 +1,10 @@
+#lang htdp/bsl
+
+
+;;; Compare the template for `contains-flatt?` with the one for `how-many`. Ignoring the
+;;; function name, they are the same. Explain the similarity.
+
+;;; A:
+;;;
+;;; The templates are the same because they deal with the same type of data:
+;;; a List-of-strings.
