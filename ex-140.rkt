0e6f41e1800248b5897ddfe4cd279dc2f6eae8cd
Ricardo Vela <aricav96@gmail.com>
2025-12-03 22:30:52 -0600

    ex-140.rkt: define one-true
    I skipped over this part woops

--- ex-140.rkt
+++ ex-140.rkt
@@ -14,3 +14,17 @@
   (cond [(empty? bs) #true]
         [(cons? bs)
          (and (first bs) (all-true (rest bs)))]))
+
+;;; Now design `one-true`, a function that consumes a list of Boolean values and determines
+;;; whether at least one item on the list is #true.
+
+; one-true: List-of-booleans -> Boolean
+; Returns true if at least one element of `bs` is #true
+(check-expect (one-true '()) #false)
+(check-expect (one-true (cons #true '())) #true)
+(check-expect (one-true (cons #false (cons #false (cons #true (cons #false '()))))) #true)
+(check-expect (one-true (cons #false (cons #false '()))) #false)
+(define (one-true bs)
+  (cond [(empty? bs) #false]
+        [else
+         (or (first bs) (one-true (rest bs)))]))
