4c6500b4d666726cfa4ed41e0915927c667c346b
Ricardo Vela <aricav96@gmail.com>
2025-12-03 22:36:29 -0600

    ex-148.rkt

---
+++ ex-148.rkt
@@ -0,0 +1,10 @@
+#lang htdp/bsl
+
+;;; Compare the function definitions from this section (`sum`, `how-many`, `all-true`,
+;;; `one-true`) with the corresponding function definitions from the preceding sections.
+;;;
+;;; Is it better to work with data definitions that ccomodate empty lists as opposed to
+;;; definitions for non-empty lists? Why? Why not?
+
+;;; A:
+;;; "Parse, don't validate" (-:
