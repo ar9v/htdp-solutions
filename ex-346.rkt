07d748ac1c2bfb2e60442d81701c9d200def616f
Ricardo Vela <aricav96@gmail.com>
2026-01-26 20:56:03 -0600

    ex-346.rkt

---
+++ ex-346.rkt
@@ -0,0 +1,8 @@
+#lang htdp/isl+
+
+;;; Formulate a data definition for the class of values to which a representation of a
+;;; BSL can evaluate
+;;;
+;;; A:
+;;; For now, a BSL-Value is a Number. As we continue to add more types, this definition
+;;; will broaden.
