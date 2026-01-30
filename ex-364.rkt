6441b6ad5599f0b90be8611c09f52662d827123e
Ricardo Vela <aricav96@gmail.com>
2026-01-29 21:20:20 -0600

    ex-364.rkt

--- ex-364.rkt
+++ ex-364.rkt
@@ -9,3 +9,8 @@
 '(ul
   (li (word) (word))
   (li (word)))
+
+;;; Which one could be represented in Xexpr.v0 or Xexpr.v1?
+;;;
+;;; A: The first one can only be expressed in terms of Xexpr.v2, but the second one could
+;;;    also be expressed in terms of Xexpr.v1
