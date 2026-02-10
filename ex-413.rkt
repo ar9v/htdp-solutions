b5c88ccab3ed6fa48e92ffe7916db2102f22196b
Ricardo Vela <aricav96@gmail.com>
2026-02-09 21:57:10 -0600

    refactor ex-412 and ex-413 with `match`
    Plus some other cleanups

--- ex-413.rkt
+++ ex-413.rkt
@@ -1,5 +1,7 @@
 #lang htdp/isl+
 
+(require 2htdp/abstraction)
+
 ;;; Design `inex*`. The function multiplies two Inex representations of numbers, including
 ;;; inputs that force an additional increase of the output's exponent. Like `inex+`, it
 ;;; must signal its own error if the result is out of range, not rely on `create-inex` to
@@ -31,33 +33,29 @@
 (check-error (inex* (create-inex 1 -1 99) (create-inex 1 -1 2)))
 (check-error (inex* (create-inex 2 1 99) (create-inex 1 1 2)))
 (define (inex* i1 i2)
-  (local [(define (normalize i)
-            (local [(define m (inex-mantissa i))
-                    (define s (inex-sign i))
-                    (define e (inex-exponent i))]
-              (if (< m 99)
-                  i
-                  (normalize (make-inex (round (/ m 10)) s (+ s (* s s e)))))))
-          (define m1 (inex-mantissa i1))
-          (define m2 (inex-mantissa i2))
-          (define s1 (inex-sign i1))
-          (define s2 (inex-sign i2))
-          (define e1 (inex-exponent i1))
-          (define e2 (inex-exponent i2))
-          (define signed-exp-sum (+ (* s1 e1) (* s2 e2)))
-          (define res-sign (if (positive? signed-exp-sum) 1 -1))
-          (define normalized
-            (normalize (make-inex (* m1 m2) res-sign (abs signed-exp-sum))))]
-    (cond [(inex< normalized MIN-POSITIVE) (error "Error: the multiplication underflowed!")]
-          [(inex> normalized MAX-POSITIVE) (error "Error: the multiplication overflowed!")]
-          [else normalized])))
+  (match (list i1 i2)
+    [(list (inex m1 s1 e1) (inex m2 s2 e2))
+     (local [(define (normalize i)
+               (match i
+                 [(inex m s e)
+                  (if (< m 99)
+                      i
+                      (normalize (make-inex (round (/ m 10)) s (abs (add1 (* s e))))))]))
+             (define signed-exp-sum (+ (* s1 e1) (* s2 e2)))
+             (define res-sign (if (positive? signed-exp-sum) 1 -1))
+             (define normalized
+               (normalize (make-inex (* m1 m2) res-sign (abs signed-exp-sum))))]
+       (cond [(inex< normalized MIN-POSITIVE)
+              (error "Error: the multiplication underflowed!")]
+             [(inex> normalized MAX-POSITIVE)
+              (error "Error: the multiplication overflowed!")]
+             [else normalized]))]))
 
 ; inex->number: Inex -> Number
 ; converts an inex into its numeric equivalent
 (define (inex->number an-inex)
   (* (inex-mantissa an-inex)
-     (expt
-      10 (* (inex-sign an-inex) (inex-exponent an-inex)))))
+     (expt 10 (* (inex-sign an-inex) (inex-exponent an-inex)))))
 
 ; inex<: Inex Inex -> Boolean
 ; Does i1 represent a smaller number than i2?
